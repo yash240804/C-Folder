62dbf9bde5e7f307d6095fddafd823b5fbe2c2e7
Yash <yashbisen24@gmail.com>
2023-12-18 18:59:46 +0530

    update

--- structure/structure2.c
+++ structure/structure2.c
@@ -8,16 +8,22 @@ struct student
 
 int main()
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 3; i++)
     {
+        printf("Enter roll no. of student no. %d\n", (i + 1));
         scanf("%d", &s1[i].rno);
+        printf("Enter Mobile no. of student no. %d\n", (i + 1));
         scanf("%d", &s1[i].mobile);
+        printf("Enter name of student no. %d\n", (i + 1));
         scanf("%s", s1[i].name);
     }
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 3; i++)
     {
-        printf("%d\n", s1[i].mobile);
+        printf("The roll no. of student no. %d\n", (i + 1));
         printf("%d\n", s1[i].rno);
+        printf("The Mobile no. of student no. %d\n", (i + 1));
+        printf("%d\n", s1[i].mobile);
+        printf("The name of student no. %d\n", (i + 1));
         printf("%s\n", s1[i].name);
     }
 
